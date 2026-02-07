72daecfac67d6c725aa7bf1879d3b429ccb280e3
Zach <zacharyamassey@gmail.com>
2026-02-07 14:48:52 -0500

    added full crud for stageplot controller

--- server/src/controllers/stageplot.ts
+++ server/src/controllers/stageplot.ts
@@ -58,17 +58,16 @@ export const getAllStagePlotsByProjectId = async (req: Request, res: Response): 
   try {
     const { id } = req.params;
     const plots: StagePlotDB[] = await db(plotTable).select('*').where({ id_prj_stp: id });
-    
+
     if (plots.length === 0) {
       res.status(404).json({
         error: 'No plots associated with that project id',
       });
       return;
     }
-    
+
     const apiPlots = plots.map(dbStagePlotToApi);
     res.json(apiPlots);
-    
   } catch (error) {
     console.error('unable to retrieve stage plots with that project ID', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -97,8 +96,76 @@ export const createStagePlot = async (req: Request, res: Response): Promise<void
 
     const [plots]: StagePlotDB[] = await db(plotTable).insert(plotDbData).returning('*');
 
-    res.json(plots);
+    res.json(dbStagePlotToApi(plots));
   } catch (error) {
-    console.error("unable to createStagePlot", error);
+    console.error('unable to createStagePlot', error);
   }
+};
+
+/**
+ * PUT /api/stageplots/:id
+ * updated stageplot by id
+ */
+
+export const updateStagePlot = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { projectId, stageId, name } = req.body;
+    if (!projectId || !stageId || !name) {
+      res.status(404).json({ error: 'Missing one or more required fields' });
+    }
+
+    const plotDbData = apiStagePlotToDb({
+      projectId,
+      stageId,
+      name,
+    });
+
+    const [results]: StagePlotDB[] = await db(plotTable)
+      .update(plotDbData)
+      .where({
+        id_stp: id,
+      })
+      .returning('*');
+
+    res.json(dbStagePlotToApi(results));
+  } catch (error) {
+    console.error('Unable to updated stage plot', error);
+    res.status(500).json({
+      error: 'Unable to updated stage plot',
+    });
+  }
+};
+
+/**
+ * DELETE /api/stageplots/:id
+ * delete stageplot by id
+ */
+
+export const deleteStagePlot = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const exists = await db(plotTable)
+      .select('*')
+      .where({
+        id_stp: id,
+      })
+      .first();
+
+    if (!exists) {
+      res.status(404).json({
+        message: 'There is no stage plot with that ID',
+      });
+    }
+
+    const [results]: StagePlotDB[] = await db(plotTable)
+      .delete()
+      .where({
+        id_stp: id,
+      })
+      .returning('*');
+
+    res.json(dbStagePlotToApi(results));
+  } catch (error) {}
 };
--- server/src/routes/stageplots.ts
+++ server/src/routes/stageplots.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
-import { getAllStagePlots, getStagePlotById, getAllStagePlotsByProjectId, createStagePlot } from '../controllers/stageplot';
+import {
+  getAllStagePlots,
+  getStagePlotById,
+  getAllStagePlotsByProjectId,
+  createStagePlot,
+  updateStagePlot,
+  deleteStagePlot,
+} from '../controllers/stageplot';
 
 const stagePlotRouter = Router();
 
 stagePlotRouter.get('/', getAllStagePlots);
 
 //Need to test and make sure this route works in BRUNO
-stagePlotRouter.get('/projects/:id', getAllStagePlotsByProjectId)
+stagePlotRouter.get('/projects/:id', getAllStagePlotsByProjectId);
 stagePlotRouter.get('/:id', getStagePlotById);
-
-
+stagePlotRouter.post('/', createStagePlot);
+stagePlotRouter.put('/:id', updateStagePlot);
+stagePlotRouter.delete('/:id', deleteStagePlot);
 
 stagePlotRouter.post('/', createStagePlot);
 
